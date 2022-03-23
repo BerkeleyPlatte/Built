70e156be3cfc95e191021b89008a18fc9239d8ad
berkeley platte <berkeleyplatte989@gmail.com>
2022-03-23 10:50:57 -0500

    image built successfully, failed to run

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
